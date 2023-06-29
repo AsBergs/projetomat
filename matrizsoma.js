d9302491370095db3b3b722e09a5f2d4fc325fce
AsBergs <anasofiabergsoares06@gmail.com>
2023-06-29 09:14:47 -0300

    EUDESISTODESSAPORCARIA

--- matrizsoma.js
+++ matrizsoma.js
@@ -1,12 +1,15 @@
-const matriz = [12.381, 10.325, 12.347, 10.432];
-const matriz2 = [5.670, 4.549, 5.728, 4.516];
+
+const matriz1 = [10, 20, 30, 40];
+const matriz2 = [50, 60, 70, 80];
 const matriz3 = [];
 
 let soma = 0;
 
-for(let numero of matriz && matriz2){
- soma =  numero + numero;
-matriz3.push(soma);
+for(let contador = 1; contador <= matriz1.length; contador++){
+for(let numero of matriz1 && matriz2){
+soma =  matriz1 + matriz2;
+matriz3.push(soma)
+}
 }
 
-document.write(`Matriz 1 = ${matriz} </br> Matriz 2 = ${matriz2} </br> Soma das matrizes = ${matriz3}`);
+document.write(`Matriz 1 = ${matriz1} </br> Matriz 2 = ${matriz2} </br> Soma das matrizes = ${matriz3}`);
